3bd327961a960f5469521021a379a0a0c04eb4d5
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2021-11-09 14:45:47 +0100

    Moved Shaders to different files

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
-﻿using GLFW;
+﻿using System;
+using System.IO;
+using System.Net;
+using GLFW;
 using static OpenGL.Gl;
 
 namespace SharpEngine
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            // Initialize and configure
+        static void Main(string[] args) {
+            // initialize and configure
             Glfw.Init();
             Glfw.WindowHint(Hint.ClientApi, ClientApi.OpenGL);
             Glfw.WindowHint(Hint.ContextVersionMajor, 3);
@@ -17,61 +19,38 @@ namespace SharpEngine
             Glfw.WindowHint(Hint.OpenglForwardCompatible, Constants.True);
             Glfw.WindowHint(Hint.Doublebuffer, Constants.False);
 
-            // Create and launch the window
+            // create and launch a window
             var window = Glfw.CreateWindow(1024, 768, "SharpEngine", Monitor.None, Window.None);
             Glfw.MakeContextCurrent(window);
             Import(Glfw.GetProcAddress);
-
-            float[] vertices = new float[]
-            {
-                -.5f, -.5f, 0,
-                .5f, -.5f, 0,
-                0f, .5f, 0,
+            
+            float[] vertices = new float[] {
+                -.5f, -.5f, 0f,
+                .5f, -.5f, 0f,
+                0f, .5f, 0f
             };
 
-            // Load the vertices into a buffer
+            // load the vertices into a buffer
             var vertexArray = glGenVertexArray();
             var vertexBuffer = glGenBuffer();
             glBindVertexArray(vertexArray);
             glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-
-            unsafe
-            {
-                fixed (float* vertex = &vertices[0])
-                {
-                    glBufferData(GL_ARRAY_BUFFER, sizeof(float) *vertices.Length, vertex, GL_STATIC_DRAW);
+            unsafe {
+                fixed (float* vertex = &vertices[0]) {
+                    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.Length, vertex, GL_STATIC_DRAW);
                 }
-                glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), null);
+                glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), NULL);
             }
+            glEnableVertexAttribArray(0);
 
-            string vertexShadersource = @"
-            #version 330 core
-            in vec3 pos;
-
-            void main()
-            {
-                gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
-            }   
-            ";
-            
-            // Create vertex shader
+            // create vertex shader
             var vertexShader = glCreateShader(GL_VERTEX_SHADER);
-            glShaderSource(vertexShader, vertexShadersource);
+            glShaderSource(vertexShader, File.ReadAllText("Shader/red-triangle.vert"));
             glCompileShader(vertexShader);
-            
-            string fragmentShaderSource = @"
-            #version 330 core
-            out vec4 result;
-
-            void main()
-            {
-                result = vec4(1, 0, 0, 1);
-            }
-            ";
             
             // create fragment shader
             var fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-            glShaderSource(fragmentShader, fragmentShaderSource);
+            glShaderSource(fragmentShader, File.ReadAllText("Shader/red-triangle.frag"));
             glCompileShader(fragmentShader);
             
             // create shader program - rendering pipeline
@@ -81,12 +60,9 @@ namespace SharpEngine
             glLinkProgram(program);
             glUseProgram(program);
             
-            glEnableVertexAttribArray(0);
-            
-            // Rendering Loop
-            while (!Glfw.WindowShouldClose(window))
-            {
-                Glfw.PollEvents();
+            // engine rendering loop
+            while (!Glfw.WindowShouldClose(window)) {
+                Glfw.PollEvents(); // react to window changes (position etc.)
                 glDrawArrays(GL_TRIANGLES, 0, 3);
                 glFlush();
             }
